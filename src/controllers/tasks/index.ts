2a6cb72addb84cb1ac9c2c8ae8f5cd079d52c894
Tosyngy <tosyngy@gmail.com>
2023-08-30 13:03:55 +0100

    implement POST task controller

--- src/controllers/tasks/index.ts
+++ src/controllers/tasks/index.ts
@@ -1,12 +1,33 @@
 import { Response, Request } from "express"
 import { ITask } from "./../../types/task"
-import Todo from "../../models/task"
+import Task from "../../models/task"
 
-const getTodos = async (req: Request, res: Response): Promise<void> => {
+const getTasks = async (req: Request, res: Response): Promise<void> => {
   try {
-    const todos: ITask[] = await Todo.find()
-    res.status(200).json({ todos })
+    const tasks: ITask[] = await Task.find()
+    res.status(200).json({ tasks })
   } catch (error) {
     throw error
   }
 }
+
+const addTask = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const body = req.body as Pick<ITask, "name" | "description" | "status">
+  
+      const task: ITask = new Task({
+        name: body.name,
+        description: body.description,
+        status: body.status,
+      })
+  
+      const newTask: ITask = await task.save()
+      const allTasks: ITask[] = await Task.find()
+  
+      res
+        .status(201)
+        .json({ message: "Task added", todo: newTask, tasks: allTasks })
+    } catch (error) {
+      throw error
+    }
+  }
